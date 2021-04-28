64f214e88cadf738f265a78eeb6a274d1c819312
Angelo Erasmo <>
2021-04-28 17:36:09 +0000

    front end final (lets go backend)

--- js/livereload.js
+++ js/livereload.js
@@ -24,7 +24,6 @@
 
 				return;
 			}
-
 			if(obj.type == 'css'){
 
 				$('link').each(function(){
--- js/script.js
+++ js/script.js
@@ -5,3 +5,17 @@ $(function(){
 $(function(){
   $("#nav-footer").load("/pages/templates/footer.html");
 });
+
+document.querySelector('.product-small-drones').addEventListener('mouseover', () => {
+  document.querySelector('#drones').classList.toggle('hide');
+}) 
+document.querySelector('.product-small-drones').addEventListener('mouseout', () => {
+  document.querySelector('#drones').classList.toggle('hide');
+}) 
+
+document.querySelector('.product-small-education').addEventListener('mouseover', () => {
+  document.querySelector('#education').classList.toggle('hide');
+}) 
+document.querySelector('.product-small-education').addEventListener('mouseout', () => {
+  document.querySelector('#education').classList.toggle('hide');
+}) 
